de361ac9c6538487ba73fa43d2829bf42caf4979
domoboy <nkonoplyannikov@gmail.com>
2019-03-07 13:16:54 +0300

    попутно осваиваем Git

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -1,1 +1,13 @@
-# 1.	Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+# Найти сумму и произведение цифр трехзначного числа введенного пользователем
+
+user_number = input('Введите трехзначное число: ')
+
+multiplication_of_number = 1
+sum_of_numbers = 0
+
+for el in user_number:
+    multiplication_of_number *= int(el)
+    sum_of_numbers += int(el)
+
+print(multiplication_of_number)
+print(sum_of_numbers)
