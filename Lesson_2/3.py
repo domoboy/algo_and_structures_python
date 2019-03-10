5a1b8475a10ec02c8a1cf458769fd92741e80760
domoboy <nkonoplyannikov@gmail.com>
2019-03-10 15:17:40 +0300

    Решена задача 3 при помощи рекурсии

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,3 +3,19 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+num = int(input('Введите число: '))
+
+def inverted_number(n):
+    if n < 10:  # базовый случай, если введена число из одной цифры
+        print(int(n), end='')
+        return n
+    else:
+        print(int(n) % 10, end='')
+        return inverted_number(n / 10)
+
+inverted_number(num)
+print()
+
+# Для проверки
+# inverted_number(3486)
+# print()
