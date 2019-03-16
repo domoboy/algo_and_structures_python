feafe1524ae7c3da40b7c34fb7ae0f1ba4f12003
domoboy <nkonoplyannikov@gmail.com>
2019-03-16 13:54:41 +0300

    задача 8 - решено

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,23 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+x = 5
+y = 4
+
+data_array = []
+
+for i in range(y):
+    row = []
+    sum_array = 0
+    print(f'Заполняем данные для строки №: {i + 1}')
+
+    for j in range(x - 1):
+        num = int(input(f'{j + 1}: '))
+        sum_array += num
+        row.append(num)
+
+    row.append(sum_array)
+    data_array.append(row)
+
+for i in data_array:
+    print(i)
